d89cbd746279d1f9d6dcb9cda0e6f63848e94f40
Bhavishaya Bansal <100150088+bansalbhavishaya@users.noreply.github.com>
2022-04-15 14:29:37 +0530

    Update class1.py

--- Week-4/class1.py
+++ Week-4/class1.py
@@ -1,1 +1,37 @@
+#EXCEPTION HANDLING
 
+try:
+  try to sum this code 
+except:
+  run it if an exception occurs
+else:
+  run it if no exception occurs
+finally:
+  always sum it 
+
+Exceptions are the errors which occur at rum time
+
+Example-
+
+try:
+  c=a/b
+except:
+  print('division not possible')
+else:
+  print(c)
+  
+  
+Example-
+  
+try:
+  age= int(input("enter the age: "))
+  if age<18:
+    raise ValueError
+  else:
+    print("age is valid")
+    
+except ValueError:
+  print("age is not valid")
+  
+finally:
+  print("this is a final clause")
